a9d7e8ce1d2717560570c762dfb497a621836823
vuongng <vuong.nguyen@ipricegroup.com>
2021-04-13 22:30:36 +0800

    Init project

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 import { getProducts } from './api/productService'
 
 function MyApp({ Component, pageProps, products }) {
@@ -7,14 +8,18 @@ function MyApp({ Component, pageProps, products }) {
     <div className="container">
       {
         products.map((item) => {
-          <div className="product-item">
-            <img src={item.imageUrl} className="cover"></img>
-            <h3>{item.name}</h3>
-            <p>{item.description}</p>
-            <p><b>{item.price}</b></p>
-            <span>{item.brand}</span>
-            <span>{item.category}</span>
-          </div>
+          <Link href=`/products/${item.id}`>
+            <a>
+              <div className="product-item">
+                <img src={item.imageUrl} className="cover"></img>
+                  <h3>{item.name}</h3>
+                  <p>{item.description}</p>
+                  <p><b>{item.price}</b></p>
+                  <span>{item.brand}</span>
+                  <span>{item.category}</span>
+                </div>
+            </a>
+          </Link>
         })
       }  
     </div>
