53c15e760b955b7f7949bfb0052d56d8f00770d7
vuongng <vuong.nguyen@ipricegroup.com>
2021-04-29 23:20:10 +0800

    Add LocalStorage

--- web/contexts/CartContext.js
+++ web/contexts/CartContext.js
@@ -1,31 +1,44 @@
-import { createContext, useReducer } from "react";
-import { getProducts } from "../pages/api/productService";
-import { CartReducer, ADD_PRODUCT, DELETE_PRODUCT } from '../reducers/cartReducer'
+/* global localStorage */
 
-const CartContext = createContext({
-    cart: [],
-    addToCart: product => {},
-    deleteCartItem: productId => {}
-});
+import { createContext, useReducer, useState, useEffect } from "react";
+import { getProducts } from "../pages/api/productService";
+import { CartReducer, ADD_PRODUCT, DELETE_PRODUCT, FETCH_CART } from '../reducers/cartReducer'
+// import { LocalStorage } from 'node-localstorage'
+
+let initialCart = []
+
+const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
+
+  // const [cart, setCart] = useState([])
+
+  // not available in functional component => only in class base component
+  // const componentDidMount = () => {
+  //   initialCart = JSON.parse(localStorage.getItem('cart')) ?? []
+  // }
+
   const [cartState, dispatch] = useReducer(CartReducer, { cart: [] })
+
+  useEffect(() => {
+    dispatch({ type: FETCH_CART })
+  }, [])
 
   const addToCart = (product) => {
     setTimeout(() => {
-        dispatch({
-            type: ADD_PRODUCT,
-            product: product
-        })
+      dispatch({
+        type: ADD_PRODUCT,
+        product: product
+      })
     }, 500)
   }
 
   const deleteCartItem = (productId) => {
     setTimeout(() => {
-        dispatch({
-            type: DELETE_PRODUCT,
-            productId: productId
-        })
+      dispatch({
+        type: DELETE_PRODUCT,
+        productId: productId
+      })
     }, 500)
   }
 
--- web/pages/api/auth/[...nextauth].js
+++ web/pages/api/auth/[...nextauth].js
@@ -7,7 +7,7 @@ export default NextAuth({
         Provider.Google({
             clientId: `${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}`,
             clientSecret: `${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET}`,
-            authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
+            // authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code',
         })
     ],
     callbacks: {
