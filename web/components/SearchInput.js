39c6fb7041b5b6c97da94852134280b693f0c104
vuongng <vuong.nguyen@ipricegroup.com>
2021-04-30 14:37:46 +0800

    Update search input styling

--- web/components/SearchInput.js
+++ web/components/SearchInput.js
@@ -23,9 +23,16 @@ const SearchInput = () => {
                                 type="text" id="searchTerm"
                                 name="searchTerm"
                                 onChange={(e) => handleSearch(value, e.target.value)} />
-                            <button type="submit" className="absolute right-0 top-0 mt-5 mr-4">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="text-gray-600 h-4 w-4 fill-current" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                            <button type="submit"
+                                className="absolute right-0 top-0 mt-5 mr-4">
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                className="text-gray-600 h-4 w-4 fill-current"
+                                fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                    <path strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                                 </svg>
                             </button>
                         </div>
